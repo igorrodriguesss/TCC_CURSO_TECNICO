280b2b60ac12d2bed981f7a09c2be9f0793faa2f
Igor Rodrigues <48895884+igorrodriguesss@users.noreply.github.com>
2021-07-27 21:12:15 -0300

    Delete etech-jobs-main directory

--- etech-jobs-main/sair.php
+++
@@ -1,6 +0,0 @@
-<?php
-	ob_start();	
-	$_SESSION['logado'] = 0;
-	//redireciona o usuario para a página de login
-	echo '<script>window.location.href = "index.php";</script>';
-?>
--- etech-jobs-main/sairWeb.php
+++
@@ -1,6 +0,0 @@
-<?php
-	ob_start();	
-	$_SESSION['logadoWeb'] = 0;
-	//redireciona o usuario para a página de login
-	echo '<script>window.location.href = "admin.php";</script>';
-?>
